59918626be520e97c8d2420f7def8cc7b29d44ce
FatiBabayeva <fm.babayeva@gmail.com>
2019-01-01 15:57:18 +0400

    service added

--- src/main/java/az/task/management/rest/TaskController.java
+++ src/main/java/az/task/management/rest/TaskController.java
@@ -1,6 +1,8 @@
 package az.task.management.rest;
 
 import az.task.management.model.Task;
+import az.task.management.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,31 +15,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class TaskRestController {
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping("/tasks")
-    public List<Task> getTasks() {
-        return null;
+    public List<Task> getTaskList() {
+        return taskService.getTaskList();
     }
 
     @GetMapping("/tasks/{id}")
     public Task getTaskById(@PathVariable Long id) {
-        return null;
+        return taskService.getTaskById(id);
     }
 
     @PostMapping("/tasks")
-    public Task addTask(@RequestBody Task task) {
-        return null;
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
     }
 
     @PutMapping("/tasks/{id}")
-    public void updateTask(@PathVariable Long id){
-
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task){
+       return taskService.updateTask(id, task);
     }
 
     @DeleteMapping("/tasks/{id}")
     public void deleteTask(@PathVariable Long id) {
-
+        taskService.deleteTask(id);
     }
 
 }
