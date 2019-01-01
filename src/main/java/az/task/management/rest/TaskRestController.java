36b3d279173a2c6c29e07760b6067552ed826156
FatiBabayeva <fm.babayeva@gmail.com>
2019-01-01 14:55:11 +0400

    rest skeleton

--- src/main/java/az/task/management/rest/TaskRestController.java
+++ src/main/java/az/task/management/rest/TaskRestController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
-
 import java.util.List;
 
 @RestController
