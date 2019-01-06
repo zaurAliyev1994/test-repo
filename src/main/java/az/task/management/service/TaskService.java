0eac92608f1b471a8a87511d2df35c8e148831f6
FatiBabayeva <fm.babayeva@gmail.com>
2019-01-06 19:33:12 +0400

    service tests added

--- src/main/java/az/task/management/service/TaskService.java
+++ src/main/java/az/task/management/service/TaskService.java
@@ -6,6 +6,8 @@ import az.task.management.mapper.TaskMapper;
 import az.task.management.model.TaskDto;
 import az.task.management.model.enums.TaskStatus;
 import az.task.management.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private static final Logger log = LoggerFactory.getLogger(TaskService.class);
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
@@ -28,7 +31,7 @@ public class TaskService {
 
     public List<TaskDto> getTaskList() {
         List<TaskEntity> taskEntities = taskRepository.findAll();
-
+        log.info("Get task list");
         return taskEntities.stream().filter(Objects::nonNull)
                 .map((taskEntity) -> TaskMapper.INSTANCE.taskEntityToDto(taskEntity)).collect(Collectors.toList());
     }
@@ -38,6 +41,7 @@ public class TaskService {
         TaskEntity taskEntity = taskRepository.findById(id)
                 .orElseThrow(() -> new TaskNotFoundException (String.format("Task with id=%s not found", id)));
 
+        log.info(String.format("Get task by id=%s",id));
         return TaskMapper.INSTANCE.taskEntityToDto(taskEntity);
     }
 
@@ -48,19 +52,21 @@ public class TaskService {
         taskEntity.setDone(false);
         taskEntity.setCreateDate(LocalDateTime.now());
         taskEntity.setUpdateDate(LocalDateTime.now());
-        return TaskMapper.INSTANCE.taskEntityToDto(taskRepository.save(taskEntity));
+        TaskEntity createdTask = taskRepository.save(taskEntity);
+        log.info(String.format("Task with id=%s created", createdTask.getId()));
+        return TaskMapper.INSTANCE.taskEntityToDto(createdTask);
     }
 
     public TaskDto updateTask(@NotNull Long id, @NotNull @Valid TaskDto task){
         TaskEntity taskEntity = TaskMapper.INSTANCE.taskDtoToEntity(task);
         taskEntity.setId(id);
-        taskEntity.setDone(task.isDone());
-        taskEntity.setStatus(TaskStatus.CREATED);
         taskEntity.setUpdateDate(LocalDateTime.now());
+        log.info(String.format("Task with id=%s updated", id));
         return TaskMapper.INSTANCE.taskEntityToDto(taskRepository.save(taskEntity));
     }
 
     public void deleteTask(@NotNull Long id) {
+        log.info(String.format("Task with id=%s deleted", id));
         taskRepository.deleteById(id);
     }
 
--- src/test/java/az/task/management/service/TaskServiceTest.java
+++ src/test/java/az/task/management/service/TaskServiceTest.java
@@ -1,12 +1,17 @@
 package az.task.management.service;
 
 import az.task.management.dao.TaskEntity;
+import az.task.management.exception.TaskNotFoundException;
 import az.task.management.model.TaskDto;
 import az.task.management.model.enums.TaskStatus;
 import az.task.management.repository.TaskRepository;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.any;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,6 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -41,9 +47,26 @@ public class TaskServiceTest {
             .updateDate(LocalDateTime.now())
             .build();
 
+    TaskDto taskDtoForCreate = TaskDto.builder()
+            .name("task2")
+            .description("test task 2")
+            .build();
+
+    TaskDto taskDtoForUpdate = TaskDto.builder()
+            .id(2L)
+            .name("task3")
+            .description("test task 3")
+            .status("CREATED")
+            .isDone(true)
+            .createDate(LocalDateTime.now())
+            .updateDate(LocalDateTime.now())
+            .build();
+
     @Before
     public void setUp() {
         when(taskRepository.findAll()).thenReturn(Arrays.asList(null, taskEntity1, null));
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(taskEntity1));
+        when(taskRepository.save(any())).then(AdditionalAnswers.returnsFirstArg());
     }
 
     @Test
@@ -55,4 +78,56 @@ public class TaskServiceTest {
         assertNotNull(taskList.get(0));
         assertEquals(taskList.get(0).getName(), "task1");
     }
+
+    @Test
+    public void getTaskByIdSuccess() {
+        TaskDto task = taskService.getTaskById(1L);
+
+        verify(taskRepository, times(1)).findById(1L);
+        assertNotNull(task);
+        assertEquals(task.getName(), "task1");
+    }
+
+    @Test(expected = TaskNotFoundException.class)
+    public void getTaskByIdWithIncorrectId() {
+        TaskDto task = taskService.getTaskById(2L);
+    }
+
+    @Test
+    public void createTask() {
+        taskService.createTask(taskDtoForCreate);
+
+        verify(taskRepository, times(1)).save((argThat(taskEntity ->
+                        taskEntity.getId() == null &&
+                        taskEntity.getName().equals(taskDtoForCreate.getName()) &&
+                        taskEntity.getDescription().equals(taskDtoForCreate.getDescription()) &&
+                        taskEntity.getStatus() == TaskStatus.CREATED &&
+                        taskEntity.isDone() == false &&
+                        taskEntity.getCreateDate() != null &&
+                        taskEntity.getUpdateDate() != null
+        )));
+    }
+
+    @Test
+    public void updateTask() {
+        taskService.updateTask(2L, taskDtoForUpdate);
+
+        verify(taskRepository, times(1)).save((argThat(taskEntity ->
+                        taskEntity.getId() == 2L &&
+                        taskEntity.getName().equals(taskDtoForUpdate.getName()) &&
+                        taskEntity.getDescription().equals(taskDtoForUpdate.getDescription()) &&
+                        taskEntity.getStatus().name().equals(taskDtoForUpdate.getStatus()) &&
+                        taskEntity.isDone() == taskDtoForUpdate.isDone() &&
+                        taskEntity.getCreateDate() == taskDtoForUpdate.getCreateDate() &&
+                        taskEntity.getUpdateDate() != null
+        )));
+    }
+
+    @Test
+    public void deleteTask() {
+        taskService.deleteTask(1L);
+
+        verify(taskRepository, times(1)).deleteById(1L);
+    }
+
 }
